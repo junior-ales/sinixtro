0d3db03b3c9b6760a39d5497cff44038b3be4185
Junior Ales <edilson.ales.jr@gmail.com>
2013-02-03 19:02:47 -0200

    Prompting instructions when running the app without arguments

---
+++ executable_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require './lib/sinixtrabinator.rb'
+
+describe 'Sinixtrabinator' do
+  it 'display options if no argument is passed' do
+    Sinixtrabinator.new('').run.should == File.new('./templates/instructions.txt').read
+  end
+end
---
+++ lib/sinixtrabinator.rb
@@ -0,0 +1,9 @@
+class Sinixtrabinator
+  def initialize(command_line_arguments)
+  end
+
+  def run
+    File.read(File.join('templates', 'instructions.txt'))
+  end
+    
+end
