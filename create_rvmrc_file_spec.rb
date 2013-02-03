8a5b17f3bbaad7b5df0f59a1ef0c9a2c73b63341
Junior Ales <edilson.ales.jr@gmail.com>
2013-02-03 16:58:02 -0200

    Refactoring spec.

--- create_rvmrc_file_spec.rb
+++ create_rvmrc_file_spec.rb
@@ -3,14 +3,14 @@ require './sinixtro'
 
 describe 'Create a rvmrc file' do
   let (:project_name) { 'project_name' }
+  let (:sinixtro) { Sinixtro.new(project_name) }
 
   it 'creates the project directory ' do
-    Sinixtro.new(project_name).create_project_dir
+    sinixtro.create_project_dir
     Dir.exist?(project_name).should be_true
   end
 
   it 'creates the rvmrc file' do
-    sinixtro = Sinixtro.new(project_name)
     sinixtro.create_project_dir
     sinixtro.create_rvmrc
     rvm_file_path = File.join(project_name, '.rvmrc') 
